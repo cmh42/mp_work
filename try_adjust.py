ebf04ab66d6ac72aef19e0d6b1b35830f3c7d56c
cmh42 <charles.harris42@icloud.com>
2025-07-01 13:08:15 +0100

    Add all original files 010725

---
+++ try_adjust.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from numpy import array, arange, sin, cos, pi, sqrt
+
+
+def period_adjust(x, period):
+    int_shift = round(x / period)
+    return x - int_shift * period
+
+
+def main():
+
+    period = 2 * pi
+    x = period * (-13) + 0.1453
+    print(period_adjust(x, period))
+
+if __name__ == '__main__': 
+    main()
+
