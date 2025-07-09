143ca6fda1eb2f3b444872133cd68a02c11abf0f
Charles Harris <charles.harris42@icloud.com>
2025-07-09 21:44:43 +0100

    Modified try_adjust printing

--- try_adjust.py
+++ try_adjust.py
@@ -7,11 +7,13 @@ def period_adjust(x, period):
     int_shift = round(x / period)
     return x - int_shift * period
 
+def print_result(x):
+    print("The result is:", x)
 
 def main():
     period = 2 * pi
     x = period * (-13) + 0.1453
-    print(period_adjust(x, period))
+    print_result(period_adjust(x, period))
     print("Now finished")
     
 if __name__ == '__main__': 
